5466f19dfe2f3282eaaad60db6e77e296501da4d
sergio costa <sergio.costa@ufma.com>
2022-09-19 10:44:17 -0300

    add expression

--- eva-vm.cpp
+++ eva-vm.cpp
@@ -7,11 +7,11 @@ int main(int argc, char const *argv[])
     EvaVM vm;
 
     auto result = vm.exec (R"(
-        (+ 1 (+ 40 1))
+        (+ "ola " "mundo")
     )");
 
-    log(AS_NUMBER(result));
-    //log(AS_CPPSTRING(result));
+    //log(AS_NUMBER(result));
+    log(AS_CPPSTRING(result));
     std::cout << "All done!\n";
 
     return 0;
